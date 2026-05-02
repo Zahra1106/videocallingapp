2ec33e81b6e14323ed57e1fdb4f669668a2fcccd
Zahra1106 <zahraaamir000@gmail.com>
2026-05-02 16:05:52 +0500

    update

--- api/index.js
+++ api/index.js
@@ -1,3 +1,17 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "Server chal raha hai! ✅" });
-}
+﻿import express from "express";
+import { connectDB } from "../lib/db.js";
+
+const app = express();
+app.use(express.json());
+
+connectDB().then(() => {
+  console.log("MongoDB connected! ✅");
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "Server chal raha hai! ✅" });
+});
+
+app.listen(3000, () => {
+  console.log("Server port 3000 pe chal raha hai!");
+});
