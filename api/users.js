15d904ee46e65ac9e21cabeccb6bf2c07def9639
Zahra1106 <zahraaamir000@gmail.com>
2026-05-02 11:40:32 +0500

    fix users api

--- api/users.js
+++ api/users.js
@@ -1,4 +1,4 @@
-import { connectDB, User } from "../lib/db.js";
+﻿import { connectDB, User } from "../lib/db.js";
 
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
