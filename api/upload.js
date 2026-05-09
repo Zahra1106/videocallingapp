9bdd65a9e801b1afc8fdb765faab4ebc4e55f15e
Zahra1106 <zahraaamir000@gmail.com>
2026-05-09 16:56:10 +0500

    fix: debug cloudinary upload

--- api/upload.js
+++ api/upload.js
@@ -1,4 +1,6 @@
 import { connectDB, User } from "../lib/db.js";
+const cloudData = await cloudRes.json();
+console.log("CLOUDINARY:", JSON.stringify(cloudData)); // ✅ add
 
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
